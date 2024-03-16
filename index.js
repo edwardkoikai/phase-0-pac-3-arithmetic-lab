65b07c12fe76394e03cefc7d6ca2a045f61ebe5e
Edward Koikai <edwardkoikai@gmail.com>
2024-03-16 21:04:33 +0300

    modidifies index.js to functions

---
+++ index.js
@@ -0,0 +1,27 @@
+function add(a,b){
+    return a + b;
+
+}
+function subtract(a,b){
+    const result = a - b;
+    return result;
+}
+function multiply(a, b){
+    return a * b;
+}
+function divide(a, b){
+    return a/b;
+}
+function increment(n){
+    return(n += 1);
+}
+function decrement(n){
+    n--
+    return n;
+}
+function makeInt(n){
+    return parseInt(n, 10);
+}
+function preserveDecimal(n){
+    return parseFloat(n);
+}
